6459a180f54a8dbf058583cddcf909b4bc38d4f0
Daniel Siqueira <daniel-nitinho@hotmail.com>
2021-07-14 20:25:21 -0300

    Cep Funcional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+  const [cep, setCep] = useState({})
+
+  function handleTextChange(e) {
+    fetch(`https://viacep.com.br/ws/${e.target.value}/json/`)
+    .then(res => res.json())
+    .then(res => setCep(res))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <strong>Olá, {props.name}</strong>
+      <div>
+        Digite o cep: <input type="text" onChange={handleTextChange}/>
+        {Object.entries(cep).map((values, keys) => (
+          <h2 key={keys}><strong>{values[0]}</strong>{values[1]}</h2>
+        ))}
+      </div>
     </div>
-  );
-}
+    )
+  }
 
 export default App;
